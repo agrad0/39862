d65cbc2587611322c054970560d741fcdfefffa4
agrad0 <grad.adam.j@gmail.com>
2022-08-20 18:01:19 +0200

    v1

---
+++ main.js
@@ -0,0 +1,10 @@
+function numbers(var1, var2){
+    let dodawanie = var1+var2;
+    let odejmowanie = var1-var2;
+    let mnozenie = var1*var2;
+    console.log('Twój wynik dodawania wynosi '+ dodawanie);
+    console.log('Twój wynik odejmowania wynosi '+ odejmowanie);
+    console.log('Twój wynik mnożenia wynosi '+ mnozenie);
+}
+numbers(5,4)
+

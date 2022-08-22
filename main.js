c7784f519e25ff2ca7e58c120a959866b9bc52bd
agrad0 <grad.adam.j@gmail.com>
2022-08-22 20:13:00 +0200

    v2

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function numbers(var1, var2){
     console.log('Twój wynik dodawania wynosi '+ dodawanie);
     console.log('Twój wynik odejmowania wynosi '+ odejmowanie);
     console.log('Twój wynik mnożenia wynosi '+ mnozenie);
+    if ((dodawanie < 0) || (odejmowanie < 0) || (mnozenie < 0)){
+        return console.log("Twój wynik jest nieprawidłowy");
+    }
 }
-numbers(5,4)
-
+numbers(1,2)
